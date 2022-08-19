a489e2d9a023877b7b28ce8cbebed3e7012a27c2
Travis <travisndunaway@gmail.com>
2022-08-19 13:12:35 -0600

    commit

--- client/main.js
+++ client/main.js
@@ -1,4 +1,23 @@
 const complimentBtn = document.getElementById("complimentButton")
+const fortuneBtn = document.getElementById("fortuneCookie")
+const weatherBtn = document.getElementById("tomorrowsWeather")
+const birthdayBtn = document.getElementById("yourBirthday")
+
+const getWeather = () => {
+    axios.get("http://localhost:4000/api/weather/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -8,4 +27,16 @@ const getCompliment = () => {
     });
 };
 
+const getBirthday = () => {
+    axios.get("http://localhost:4000/api/birthday/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+
 complimentBtn.addEventListener('click', getCompliment)
+fortuneBtn.addEventListener('click', getFortune)
+weatherBtn.addEventListener('mouseover', getWeather)
+birthdayBtn.addEventListener('click', getBirthday)
